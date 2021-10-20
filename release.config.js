63840f9027a314d6dceb64305e2ddbf4c43b8291
Nikola Ninkovic <nn.ninkovicnikola@gmail.com>
2021-10-21 00:34:32 +0200

    fix: fix description

---
+++ release.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    branches: "master",
+    repositoryUrl: https://github.com/nikola-ninkovic/react-app,
+    plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github']
+}
